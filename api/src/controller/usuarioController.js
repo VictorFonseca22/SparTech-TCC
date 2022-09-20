4dd7e22e7544721b21fc454bd26f11ca4e09192b
Victor Fonseca <victor.fonseca0@gmail.com>
2022-09-20 17:52:01 -0300

    API: cadastro profissional e categoria feitas

--- api/src/controller/usuarioController.js
+++ api/src/controller/usuarioController.js
@@ -1,7 +1,7 @@
 
 import { Router } from "express";
 import multer from 'multer'
-import {cadastroCliente, cadastroProfissional} from '../repository/usuarioRepository.js'
+import {cadastroCliente, cadastroProfissional, listarCategorias} from '../repository/usuarioRepository.js'
 
 const uploadCliente = multer({ dest: 'storage/FotosCliente'})
 const uploadProfissional = multer({ dest: 'storage/FotosProfissional'})
@@ -45,5 +45,59 @@ server.post('/cadastrarCliente', async (req, resp) => {
 
     }
 })
+// Cadastrar Profissional
+server.post('/cadastrarProfissional', async (req, resp) => {
+    try{
+        const novoCadastro = req.body;
 
+        if(!novoCadastro.nome){
+            throw new Error('Nome é obrigatório!');
+        }
+
+        if(!novoCadastro.email){
+            throw new Error('Email é obrigatório!');
+        }
+
+        if(!novoCadastro.telefone){
+            throw new Error('Telefone é obrigatório!');
+        }
+
+        if(!novoCadastro.senha){
+            throw new Error('Senha é obrigatória!');
+        }
+
+        if(!novoCadastro.nascimento){
+            throw new Error('Data de nascimento é obrigatória!');
+        }
+     
+        if(!novoCadastro.cpf){
+            throw new Error('CPF é obrigatório!');
+        }
+        
+        if(!novoCadastro.servico){
+            throw new Error('Tipo de serviço é obrigatório')
+        }
+
+        const cadastro =  await cadastroProfissional(novoCadastro);
+        resp.send(cadastro)
+    } 
+    catch(err){
+        resp.status(401).send({
+            erro:err.message
+        })
+
+    }
+})
+// Listar Categorias
+server.get('/api/categoria', async (req, resp) => {
+    try {
+        const linhas = await listarCategorias();
+        resp.send(linhas);
+    }
+    catch (err) {
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
 export default server;
--- api/src/repository/usuarioRepository.js
+++ api/src/repository/usuarioRepository.js
@@ -17,13 +17,24 @@ export async function cadastroCliente (cliente){
 export async function cadastroProfissional (profissional){
     const comando = 
     `
-    insert into tb_cliente (nm_cliente, ds_email, ds_senha, ds_cpf, ds_telefone, dt_nasc)
-     values(?, ?, ?, ?, ?, ?);
+    insert into tb_profissional (nm_profissional, ds_email, ds_senha, ds_cpf, ds_telefone, dt_nasc, id_tipo_serv)
+     values(?, ?, ?, ?, ?, ?, ?);
     `
 
-    const [resposta] = await con.query(comando, [profissional.nome, profissional.email, profissional.senha, profissional.cpf, profissional.telefone, profissional.nascimento]);
+    const [resposta] = await con.query(comando, [profissional.nome, profissional.email, profissional.senha, profissional.cpf, profissional.telefone, profissional.nascimento, profissional.servico]);
  
     profissional.id = resposta.insertId;
 
-    return cliente;
+    return profissional;
+}
+
+export async function listarCategorias() {
+    const comando = `
+        select id_tipo_serv         as id,
+               nm_servico           as servico
+          from tb_tipo_serv
+    `
+
+    const [linhas] = await con.query(comando);
+    return linhas;
 }
