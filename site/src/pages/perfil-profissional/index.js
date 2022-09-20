5e164432196478266c407c23e7b6745390d6a03f
jonas da cunha madeira <jonascn.2021@gmail.com>
2022-09-20 09:26:56 -0300

    perfil-profissional

--- site/src/pages/perfil-profissional/index.js
+++ site/src/pages/perfil-profissional/index.js
@@ -7,11 +7,73 @@ export default function Perfilprofissional() {
 
             <div className='barra'>
 
-            <img className='Logo' src='/assets/images/teste final 1.png' />
+                <img src='/assets/images/voltar.png' />
+                <h1>voltar</h1>
+
+                <img className='Logo' src='/assets/images/teste final 1.png' />
+
+                <h1 className='denunciar'>denunciar</h1>
+
+                <h1 className='servico'>Serviços</h1>
 
 
 
             </div>
+
+            <div className='fundo'>
+
+                <img src='/assets/images/fundo.png' />
+
+            </div>
+
+            <div className='informações'>
+
+                <img src='/assets/images/pessoa.png' />
+
+                <h1>JANETE GONÇALVES</h1>
+
+                <p>(11) 93344-2371</p>
+
+                <p>JANJANGON@YAHOO.COM.BR</p>
+
+                <img src='/assets/images/estrela.png' />
+                <img src='/assets/images/estrela.png' />
+                <img src='/assets/images/estrela.png' />
+                <img src='/assets/images/estrela.png' />
+                <img src='/assets/images/estrela-metade.png' />
+                <p>4,5</p>
+
+                        <div className='editar'>
+
+                            <h1>editar perfíl</h1>
+                            <img src='/assets/images/caneta.png' />
+
+                        </div>
+
+                <hr/>
+
+                <div className='area de atuacao'>
+
+                    <h1>área(s) de atuação</h1>
+
+                    <p>front-end</p>
+                    <p>back-end - javascript</p>
+                    <p>back-end - banco de dados</p>
+
+                    <h1>Licenças e certificados</h1>
+
+                    <p>MCPS: Microsoft Certified Professional 2017</p>
+                    <p>instituto social nossa senhora de fátima 1908</p>
+
+                </div>
+
+                <hr/>
+
+                <h1>comentários sobre você</h1>
+
+            </div>
+
+
 
 
 
