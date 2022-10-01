181aa6e447f74377f7e1f8491fe42a2366713837
DaniloKunzendorff <danilo.k.oliveirat@gmail.com>
2022-10-01 17:14:25 -0300

    css do serviços ativos

--- site/src/pages/servicos-ativos/index.js
+++ site/src/pages/servicos-ativos/index.js
@@ -1,6 +1,15 @@
 import './index.scss'
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Serviços() {
+
+    const navigate = useNavigate()
+
+
+    function home() {
+        navigate('/')
+    }
 
     return (
         <main className='servicos'>
@@ -8,7 +17,7 @@ export default function Serviços() {
             <header className="barra">
 
                 <div>
-                    <img className='logo' src='/assets/images/teste final 1.png' />
+                    <img onClick={home} className='logo' src='/assets/images/teste final 1.png' />
                 </div>
 
                 <h1 className="ativos">serviços ativos</h1>
@@ -22,91 +31,101 @@ export default function Serviços() {
 
             <section className="ordem">
 
-                <div className='informacoes'>
+                <section className="ajuste">
 
-                    <img src='./assets/images/japones.png' />
+                    <div className='informacoes'>
 
-                    <div className='ramo'>
-                        <h1>Jonas Madeira</h1>
+                        <img src='./assets/images/japones.png' />
 
-                        <p>especialista em front-end suporte técnico</p>
+                        <div className='ramo'>
+                            <h1>Jonas Madeira</h1>
+
+                            <p>especialista em front-end suporte técnico</p>
+                        </div>
+
+                        <div className="dois">
+                            <div className='info'>
+                                <h2>serviço a realizar</h2>
+
+                                <p>reparar computador do cliente. possível troca de fonte de alimentação.</p>
+                            </div>
+
+                            <div className='info'>
+                                <h2>valor a ser pago</h2>
+
+                                <p>R$ 150,00</p>
+                            </div>
+
+                        </div>
+
+                        <div className="dois">
+
+                            <div className='endeinforeco'>
+                                <h2>endereço do serviço</h2>
+
+                                <p>rua joaquim mendes feliz, 349 - embu-guaçú - são paulo</p>
+                            </div>
+
+                            <div className='info'>
+                                <h2>data de entrega marcada</h2>
+
+                                <p>22-08-2022</p>
+                            </div>
+
+                        </div>
+
+                        <div className="dois">
+
+                            <div className='info'>
+                                <h2>data limite</h2>
+
+                                <p>11-09-2022</p>
+                            </div>
+
+                            <div className='info'>
+                                <h2>situação do serviço</h2>
+
+                                <p>concluído</p>
+                            </div>
+
+                        </div>
+
                     </div>
 
-                    <div className='servico'>
-                        <h1>serviço a realizar</h1>
+                    <div className='resumo'>
 
-                        <p>reparar computador do cliente. possível troca de fonte de alimentação.</p>
+                        <h1>resumo do serviço ativo</h1>
+
+                        <hr />
+
+                        <div className='valor'>
+                            <p>valor total do serviço ativo</p>
+
+                            <p>r$ 150,00</p>
+                        </div>
+
+
+
+                        <hr />
+
+                        <div className="button">
+                            <button className='pagar'>
+                                pagar serviço concluído
+                            </button>
+
+                            <button className='cancelar'>
+                                cancelar serviço
+                            </button>
+                        </div>
+                        <h5>A EsparTech, irá dar a garantia de 1 mês
+                            sobre o serviço realizado, caso aconteça algum problema, a EsparTech irá mandar um profissional sem necessidade de um novo pagamento. </h5>
+
+
                     </div>
 
-                    <div className='valor'>
-                        <h1>valor a ser pago</h1>
-
-                        <p>R$ 150,00</p>
-                    </div>
-
-                    <div className='endereco'>
-                        <h1>endereço do serviço</h1>
-
-                        <p>rua joaquim mendes feliz, 349 - embu-guaçú - são paulo</p>
-                    </div>
-
-                    <div className='entrega'>
-                        <h1>data de entrega marcada</h1>
-
-                        <p>22-08-2022</p>
-                    </div>
-
-                    <div className='data'>
-                        <h1>data limite</h1>
-
-                        <p>11-09-2022</p>
-                    </div>
-
-                    <div className='situação'>
-                        <h1>situação do serviço</h1>
-
-                        <p>concluído</p>
-                    </div>
-
-                </div>
-
-                <div className='resumo'>
-
-                    <h1>resumo do serviço ativo</h1>
-
-                    <hr />
-
-                    <div className='valor'>
-                        <p>valor total do serviço ativo</p>
-
-                        <p>r$ 350,00</p>
-                    </div>
-
-                    <div className='qtd'>
-                    <p>qtd. de serviço ativo</p>
-
-                    <p>2</p>
-                    </div>
-
-                    <hr />
-
-                    <button className='pagar'>
-                        pagar serviço concluído
-                    </button>
-
-                    <button className='cancelar'>
-                        cancelar serviço
-                    </button>
-
-                    <h5>A EsparTech, irá dar a garantia de 1 mês
-                        sobre o serviço realizado, caso aconteça algum problema, a EsparTech irá mandar um profissional sem necessidade de um novo pagamento. </h5>
 
 
-                </div>
-
-
-
-
+                </section>
             </section>
 
 
