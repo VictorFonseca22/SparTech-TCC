d70d6ed2c3a0c4f4f5b7bc14c114721730be4903
DaniloKunzendorff <danilo.k.oliveirat@gmail.com>
2022-11-04 18:14:40 -0300

    responsivo, page de pagamento quase feita, css do pagamento quase feita

--- site/src/index.js
+++ site/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/landing'
 import Login from './pages/Cadastros-Login/login'
 import CadastraCliente from './pages/Cadastros-Login/cadastro-cliente'
@@ -21,35 +21,38 @@ import ServicosRealizados from './pages/perfis-funcoes/servicosRealizados';
 import ServicosPendentes from './pages/perfis-funcoes/servicosPendentes';
 import PerfilCliente from './pages/perfis-funcoes/perfil-cliente';
 import ServicosContratados from './pages/perfis-funcoes/servicosContratados';
+import PagarServico from './pages/perfis-funcoes/pagar-servicos';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-         <Route path='/' element={<LandingPage />} />
-         <Route path='/login' element={<Login />} />
-         <Route path='/cadastro-cliente' element={<CadastraCliente />} />
-         <Route path='/cadastro-profissional' element={<CadastraProfissional />} />
-         <Route path='/perfil-profissional' element={<PerfilProfissional />} />
-         <Route path='/perfil-profissional/:idParam' element={<PerfilProfissional />} />
-         <Route path='/perfil-cliente/:idParam' element={<PerfilCliente />} />
-         <Route path='/busca-profissional' element={<BuscaProf />} />
-         <Route path='/servicos-ativos/:idParam' element={<ServicosAtivos />} />
-         <Route path='/solicitar-servico/:idParam' element={<SolicitarServ />} />
-         <Route path='/meus-servicos/:idParam' element={<MeusServicos />} />
-         <Route path='/menu-adm' element={<MenuADM />} />
-         <Route path='/remover-cliente' element={<RemocaoCli />} />
-         <Route path='/remover-profissional' element={<RemocaoProf />} />
-         <Route path='/remover-profissional' element={<RemocaoProf />} />
-         <Route path='/denuncias-cliente' element={<DenunCliente />} />
-         <Route path='/denuncias-cliente/:idParam' element={<DenunCliente />} />
-         <Route path='/denuncias-profissional' element={<DenunProfissional />} />
-         <Route path='/denuncias-profissional/:idParam' element={<DenunProfissional />} />
-         <Route path='/solicitacoes-de-servico/:idParam' element={<SolicitacoesServ />} />
-         <Route path='/servicos-realizados/:idParam' element={<ServicosRealizados />} />
-         <Route path='/servicos-pendentes/:idParam' element={<ServicosPendentes />} />
-         <Route path='/servicos-contratados/:idParam' element={<ServicosContratados />} />
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/cadastro-cliente' element={<CadastraCliente />} />
+        <Route path='/cadastro-profissional' element={<CadastraProfissional />} />
+        <Route path='/perfil-profissional' element={<PerfilProfissional />} />
+        <Route path='/perfil-profissional/:idParam' element={<PerfilProfissional />} />
+        <Route path='/perfil-cliente/:idParam' element={<PerfilCliente />} />
+        <Route path='/busca-profissional' element={<BuscaProf />} />
+        <Route path='/servicos-ativos/:idParam' element={<ServicosAtivos />} />
+        <Route path='/solicitar-servico/:idParam' element={<SolicitarServ />} />
+        <Route path='/meus-servicos/:idParam' element={<MeusServicos />} />
+        <Route path='/menu-adm' element={<MenuADM />} />
+        <Route path='/remover-cliente' element={<RemocaoCli />} />
+        <Route path='/remover-profissional' element={<RemocaoProf />} />
+        <Route path='/remover-profissional' element={<RemocaoProf />} />
+        <Route path='/denuncias-cliente' element={<DenunCliente />} />
+        <Route path='/denuncias-cliente/:idParam' element={<DenunCliente />} />
+        <Route path='/denuncias-profissional' element={<DenunProfissional />} />
+        <Route path='/denuncias-profissional/:idParam' element={<DenunProfissional />} />
+        <Route path='/solicitacoes-de-servico/:idParam' element={<SolicitacoesServ />} />
+        <Route path='/servicos-realizados/:idParam' element={<ServicosRealizados />} />
+        <Route path='/servicos-pendentes/:idParam' element={<ServicosPendentes />} />
+        <Route path='/servicos-contratados/:idParam' element={<ServicosContratados />} />
+        <Route path='/pagamento' element={<PagarServico />} />
+        
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ site/src/pages/perfis-funcoes/pagar-servicos/index.js
@@ -0,0 +1,177 @@
+import './index.scss';
+
+
+export default function PagarServico() {
+
+
+
+    return (
+        <main className='page-pagamento'>
+
+            <header className="barra">
+
+                <div>
+                    <img className='logo' src='/assets/images/teste final 1.png' />
+                </div>
+
+                <h1 className="ativos">serviços ativos</h1>
+
+                <div className='volta'>
+                    <img className='menu' src='/assets/images/voltar.png' />
+                    <p>voltar</p>
+                </div>
+
+            </header>
+
+
+            <section className="ordem">
+
+
+                <section className="ajuste">
+
+                    <section className='pag'>
+
+                        <div className='informacoes' >
+                            <input class="trigger-input" id="faq-titulo-2" type="radio" />
+
+
+                            <div className='mapeamento-perfil'>
+                                <img src='./assets/images/moça.png' />
+                                <div className='ramo'>
+                                    <h1>Maria</h1>
+
+                                    <p>Programação</p>
+                                </div>
+                            </div>
+
+
+
+
+
+
+                            <div className="dois">
+                                <div className='info'>
+                                    <h2>serviço a realizar</h2>
+
+                                    <p>fazer um site</p>
+                                </div>
+
+                                <div className='info'>
+                                    <h2>valor a ser pago</h2>
+
+                                    <p>R$500</p>
+                                </div>
+
+                            </div>
+
+                            <div className="dois">
+
+                                <div className='endeinforeco'>
+                                    <h2>endereço do serviço</h2>
+
+                                    <p>Rua rua, 1234 - São Paulo - SP</p>
+                                </div>
+
+                                <div className='info'>
+                                    <h2>data de entrega marcada</h2>
+
+                                    <p>22-08-2022</p>
+                                </div>
+
+                            </div>
+
+                            <div className="dois">
+
+                                <div className='info'>
+                                    <h2>data limite</h2>
+
+                                    <p>20/05/2025</p>
+                                </div>
+
+                                <div className='info'>
+                                    <h2>situação do serviço</h2>
+
+                                    <p>Pendente</p>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div className='pagamento'>
+
+                            <input type='radio' />
+
+                            <div className='cartão'>
+
+                                <img src="./assets/images/cartão.png"  />
+                                <p>cartão de crédito/debito</p>
+
+                            </div>
+
+                            <input type='radio' />
+
+                            <div className='boleto'>
+
+                                <img src='' />
+                                <p>boleto bancário</p>
+
+                            </div>
+
+                        </div>
+
+                    </section>
+
+
+                    <div className='resumo'>
+
+                        <h1>resumo do serviço ativo</h1>
+
+                        <hr />
+
+                        <div className='valor'>
+                            <p>valor total do serviço ativo</p>
+
+                            <p>r$ 150,00</p>
+
+                        </div>
+
+
+
+
+
+                        <hr />
+
+                        <div className="button">
+                            <button className='pagar'>
+                                pagar serviço concluído
+                            </button>
+
+                            <button className='cancelar'>
+                                cancelar serviço
+
+                            </button>
+                        </div>
+
+                        <h5>A EsparTech, irá dar a garantia de 1 mês
+                            sobre o serviço realizado, caso aconteça algum problema, a EsparTech irá mandar um profissional sem necessidade de um novo pagamento. </h5>
+
+
+                    </div>
+
+
+
+
+                </section>
+            </section>
+
+
+
+
+
+
+
+
+        </main>
+    );
+
+}
